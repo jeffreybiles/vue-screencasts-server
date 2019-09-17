c9b5b8147f6b1c8585e488f74a662a564caea4a5
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-17 01:07:57 -0700

    complete videos CRUD

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,20 +1,36 @@
 class Api::VideosController < ActionController::API
   def index
     videos = Video.all
-    render json: VideoSerializer.new(videos, include: [:tags]).serializable_hash
+    render json: video_json(videos)
   end
 
   def show
     video = Video.find(params[:id])
-    render json: VideoSerializer.new(video, include: [:tags]).serializable_hash
+    render json: video_json(video)
   end
 
   def create
     video = Video.create(video_params)
-    render json: VideoSerializer.new(video, include: [:tags]).serializable_hash
+    render json: video_json(video)
+  end
+
+  def destroy
+    Video.find(params[:id]).destroy
+    head 204
+  end
+
+  def update
+    video = Video.find(params[:id])
+    video.update(video_params)
+    video.save()
+    render json: video_json(video)
   end
 
   private
+
+  def video_json(video)
+    VideoSerializer.new(video, include: [:tags]).serializable_hash
+  end
 
   def video_params
     params.require(:video).permit(:name, :description, :thumbnail, :videoUrl)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
-    resources :videos, only: [:index, :show, :create]
+    resources :videos, only: [:index, :show, :create, :updae, :destroy]
     resources :tags, only: [:index, :show]
     resources :users, only: [:index]
   end
