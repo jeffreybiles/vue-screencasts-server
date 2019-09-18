429abe878bd4037f79e409618bec6954bfaa52d9
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-18 09:21:05 -0700

    Make CORS work

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module VueScreencastsServer
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
       end
     end
   end
