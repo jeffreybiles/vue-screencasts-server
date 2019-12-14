d96183d523ccc1dd2abd2a6dde2cae2f4925c966
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-14 09:43:15 -0800

    Add course difficulty

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_205314) do
+ActiveRecord::Schema.define(version: 2019_12_14_171653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_205314) do
     t.string "image_url"
     t.text "description"
     t.decimal "order"
+    t.string "difficulty"
   end
 
   create_table "tags", force: :cascade do |t|
