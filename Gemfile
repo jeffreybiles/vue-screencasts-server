05544ab66ce52e3cbf9008f8b903ee3374189e4b
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-26 15:16:54 -0600

    First email with sendgrid (replace with actual emails, using ActionMailer)

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'rack-cors'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'sendgrid-ruby'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ lib/tasks/email.rb
@@ -0,0 +1,14 @@
+require 'sendgrid-ruby'
+include SendGrid
+
+from = Email.new(email: 'test@example.com')
+to = Email.new(email: 'bilesjeffrey@gmail.com')
+subject = 'Sending with SendGrid is Fun'
+content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+mail = SendGrid::Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
