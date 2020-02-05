407e802e3ad0cfc9b5be9d177572d1b2eeb50fa1
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-05 11:43:50 -0800

    Allow free period

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,4 +6,8 @@ class Video < ApplicationRecord
   has_many :users, through: :video_plays
 
   belongs_to :course, optional: true
+
+  def in_free_period
+    (Date.today - 7) < published_at
+  end
 end
--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,13 +1,14 @@
 class VideoSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :thumbnail, :pro,
+  attributes :id, :name, :description, :thumbnail, :pro, :in_free_period,
              :created_at, :updated_at, :duration, :published_at, :code_summary, :order
   belongs_to :course
 
   attribute :videoUrl, if: Proc.new { |record, params| 
     isFree = !record.pro
     userPro = params && params[:user_pro]
-    isFree || userPro
+    freePeriod = record.in_free_period
+    isFree || userPro || freePeriod
   } do |object|
     object.videoUrl
   end
