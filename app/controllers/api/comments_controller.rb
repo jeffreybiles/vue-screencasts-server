cc72108174c80a6ec988c59f148546061ab71b71
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-14 02:03:45 -0800

    create notifications when creating comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,6 +3,19 @@ class Api::CommentsController < ApplicationController
     comment = Comment.create(comment_params)
     comment.user = current_user
     comment.save
+
+    message = "#{current_user.name} has responded to your post."
+
+    admin_user = User.find(3)
+    admin_notification = Notification.create(comment: comment, user: admin_user, content: message)
+    # admin_notification.send_email
+
+    if params[:parent_id]
+      parent_comment = Comment.find(params[:parent_id])
+      notification = Notification.create(comment: comment, user: parent_comment.user, content: message)
+      # notification.send_email
+    end
+
     render json: comment_json(comment)
   end
 
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,19 @@
 class Notification < ApplicationRecord
   belongs_to :comment
   belongs_to :user
+
+  def comment_notification_list_id
+    7
+  end
+  
+  # TODO: Make this work
+  def send_email
+    # api_instance = SibApiV3Sdk::ContactsApi.new
+
+    # result = api_instance.get_contact_info(user.email)
+    # can_receive = result.list_ids.include?(comment_notification_list_id)
+    # if can_receive
+
+    # end
+  end
 end
