f5b6e7e2f32191d53188620f99361c52aedfcdab
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-23 20:48:04 -0800

    plan saving updates

--- app/controllers/api/stripe_controller.rb
+++ app/controllers/api/stripe_controller.rb
@@ -35,8 +35,7 @@ class Api::StripeController < ApplicationController
 
     user.subscription_id = subscription.id
     user.plan_id = planId
-    user.plan_hash = subscription.plan.to_json
-    
+    user.plan_hash = subscription.plan.to_hash
     user.save
 
     render json: UserSerializer.new(user).serializable_hash
