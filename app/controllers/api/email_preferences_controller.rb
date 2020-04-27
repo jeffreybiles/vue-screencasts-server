811c75309f1c886d3e3a40cbb67e616c64cb2a1d
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-04-27 02:05:40 -0700

    Can update contact list preferences

--- app/controllers/api/email_preferences_controller.rb
+++ app/controllers/api/email_preferences_controller.rb
@@ -6,19 +6,14 @@ require 'openssl'
 class Api::EmailPreferencesController < ApplicationController
   def change_subscription
     id = params['id']
-    new_status = params['isSubscribed']
+    is_subscribed = params['isSubscribed']
 
-    begin
-      contact = api_instance.get_contact_info(current_user.email)
-
-      if new_status
-        update_subscription(current_user.email, id, 'add')
-      else
-        update_subscription(current_user.email, id, 'remove')
-      end
-    rescue
-      create_contact(current_user, [id])
-    end
+    # begin
+    result = Email.new.update_subscription(current_user, id, is_subscribed)
+    head result.status
+    # rescue
+    #   create_contact(current_user, [id])
+    # end
   end
 
   def passwordless_subscription
@@ -37,9 +32,9 @@ class Api::EmailPreferencesController < ApplicationController
   def status
     begin
       #Retrieves contact informations
-      result = Email.new.contact(current_user.email)
+      result = Email.new.contact(current_user)
       render json: {
-        contact: result,
+        contactLists: result['contactLists']
       }
     rescue SibApiV3Sdk::ApiError => e
       puts "Exception when calling ContactsApi->get_contact_info: #{e}"
@@ -48,28 +43,6 @@ class Api::EmailPreferencesController < ApplicationController
   end
 
   private
-
-  def update_subscription(email, list_id, add_or_remove)
-    begin
-      url = URI("https://api.sendinblue.com/v3/contacts/lists/#{list_id}/contacts/#{add_or_remove}")
-
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Post.new(url)
-      request["accept"] = 'application/json'
-      request["content-type"] = 'application/json'
-      request["api-key"] = ENV['SENDBLUE_API_KEY']
-      request.body = "{\"emails\":[\"#{email}\"]}"
-
-      response = http.request(request)
-      p response
-    rescue SibApiV3Sdk::ApiError => e
-      puts "Exception when calling ContactsApi->add_contact_to_list: #{e}"
-      head 500
-    end
-  end
 
   def api_instance
     SibApiV3Sdk::ContactsApi.new
--- app/models/email.rb
+++ app/models/email.rb
@@ -10,12 +10,26 @@ class Email
     p result    
   end
 
-  def contact(email)
-    get("contacts/2")['contact']
+  def contact(user)
+    get("contacts/2")
+  end
+
+  def update_subscription(user, list_id, is_subscribed)
+    post('contactLists', {
+      contactList: {
+        list: list_id,
+        contact: 2,
+        status: is_subscribed ? 1 : 2
+      }
+    })
   end
 
   def get(url, props = {})
-    JSON.parse(Faraday.get("#{base_url}/#{url}", props, {"Api-Token" => api_key}).body)
+    JSON.parse(Faraday.get("#{base_url}/#{url}", props, {"Api-Token" => api_key, "Content-Type" => "application/json"}).body)
+  end
+
+  def post(url, props = {})
+    Faraday.post("#{base_url}/#{url}", props.to_json, {"Api-Token" => api_key})
   end
 
   def base_url
