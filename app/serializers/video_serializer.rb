1811e189aefeda70d8b52466bddaa867c9f8b03a
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-07-07 21:13:15 -0700

    Server sends all urls

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -2,16 +2,7 @@ class VideoSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :description, :thumbnail, :pro, :in_free_period, :lesson_type,
              :created_at, :updated_at, :duration, :published_at, :order,
-             :code, :code_summary, :code_summary_state
+             :code, :code_summary, :code_summary_state, :videoUrl
   belongs_to :course
 
-  attribute :videoUrl, if: Proc.new { |record, params| 
-    isFree = !record.pro
-    userPro = params && params[:user_pro]
-    user_admin = params && params[:user_admin]
-    freePeriod = record.in_free_period
-    (isFree || userPro || freePeriod) && (record.released || user_admin)
-  } do |object|
-    object.videoUrl
-  end
 end
