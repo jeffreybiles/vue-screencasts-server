92fc95d0f4792b3adeeb924ffeb8dc138f1525ad
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-07-25 20:15:42 -0700

    Create new bootcamp user via API

--- app/controllers/api/training_users_controller.rb
+++ app/controllers/api/training_users_controller.rb
@@ -1,10 +1,13 @@
 class Api::TrainingUsersController < ApplicationController
+  before_action :authenticate_user
+
   def index
     users = User.where(bootcamp: true)
     render json: UserSerializer.new(users).serializable_hash
   end
 
   def create
-
+    user = User.bootcamp_user(params[:username], params[:password])
+    render json: UserSerializer.new(user).serializable_hash
   end
 end
