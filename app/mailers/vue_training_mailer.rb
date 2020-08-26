4b4646dedcfb40bb43aaa5c36f393c649744f075
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-08-25 23:53:44 -0700

    already_exists mailer

--- app/mailers/vue_training_mailer.rb
+++ app/mailers/vue_training_mailer.rb
@@ -4,4 +4,9 @@ class VueTrainingMailer < ApplicationMailer
     @password = password
     mail(to: @email, subject: 'Welcome to VueTraining!  Here\'s your login info.')
   end
+
+  def already_exists(email)
+    @email = email
+    mail(to: @email, subject: 'Welcome to VueTraining!  Here\'s your login info.')
+  end
 end
--- test/mailers/previews/vue_training_preview.rb
+++ test/mailers/previews/vue_training_preview.rb
@@ -2,4 +2,8 @@ class VueTrainingMailerPreview < ActionMailer::Preview
   def login_info
     VueTrainingMailer.login_info('test@email.com', 'password')
   end
+
+  def already_exists
+    VueTrainingMailer.already_exists('test@email.com')
+  end
 end
